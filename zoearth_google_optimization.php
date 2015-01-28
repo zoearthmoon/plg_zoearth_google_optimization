55aefae222efbaba93d28a21f5c4dc1ba67ac850
zoearthmoon@gmail.com <zoearthmoon@gmail.com>
2015-01-28 22:23:24 +0800

    新增 修改

--- zoearth_google_optimization.php
+++ zoearth_google_optimization.php
@@ -18,6 +18,12 @@ class plgSystemZoearth_Google_Optimization extends JPlugin
                 $response = str_replace('<script src=','<script async src=',$response);
             }
             
+			//20150128 針對TAG圖片處理
+            if ($this->params->get('tag_img') == '1')
+            {
+                $response = str_replace('span class="label label-success"><img','span ><img',$response);
+            }
+			
             JResponse::setBody($response);
         }
     }
